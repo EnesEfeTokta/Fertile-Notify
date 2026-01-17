1b672de7453ff4ff99dc9554df3006e6dbde8b37
Enes Efe Tokta <enesefetokta009@gmail.com>
2026-01-17 21:51:15 +0300

    Add notification and subscription infrastructure
    Removed unused Class1.cs. Added ConsoleNotificationSender for sending notifications to the console and InMemorySubscriptionRepository for managing subscriptions in memory.

---
+++ backend/FertileNotify.Infrastructure/Notifications/ConsoleNotificationSender.cs
@@ -0,0 +1,18 @@
+﻿using FertileNotify.Domain.Entities;
+using FertileNotify.Application.Interfaces;
+
+namespace FertileNotify.Infrastructure.Notifications
+{
+    public class ConsoleNotificationSender : INotificationRepository
+    {
+        public Task SendAsync(Notification notification)
+        {
+            Console.WriteLine($"=== Notification Sent ===");
+            Console.WriteLine($"ID: {notification.Id}");
+            Console.WriteLine($"Title: {notification.Title}");
+            Console.WriteLine($"Title: {notification.Message}");
+            Console.WriteLine($"Create At: {notification.CreateAt}");
+            return Task.CompletedTask;
+        }
+    }
+}
